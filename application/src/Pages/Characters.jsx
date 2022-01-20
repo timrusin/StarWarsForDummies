612f90c41505d753d805526b68dd981209328425
timrusin <timrusin@gmail.com>
2022-01-20 13:18:32 -0600

    added background working on character map again

--- application/src/Pages/Characters.jsx
+++ application/src/Pages/Characters.jsx
@@ -3,20 +3,24 @@ import '../CSS/Characters.css'
 import CharacterCard from '../Components/CharacterCard'
 
 const Characters = ()=>{
-  return(
+  
+   const [characters, setCharacters]= useState()
+     useEffect(()=> {
+        fetch('https://swapi.dev/api/people')
+        .then((res)=>res.json())
+        .then((json)=>{
+            setCharacters(json)
+        })
+    },[])
+    if(!characters) {
+        return <p>Loading.......</p>
+    }
+    console.log(characters)
+  
+    return(
       <p>This is the characters dashboard</p>
   )
-//  const [characters, setCharacters]= useState()
-//      useEffect(()=> {
-//         fetch('https://swapi.dev/api/people')
-//         .then((res)=>res.json())
-//         .then((json)=>{
-//             setCharacters(json)
-//         })
-//     },[])
-//     if(!characters) {
-//         return <p>Loading.......</p>
-//     }
+
    
 //     characters.map((item)=>{
 //         return <CharacterCard {...item}/>
