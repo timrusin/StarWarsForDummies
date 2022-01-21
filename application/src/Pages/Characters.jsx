ba2cb8f3284b023ed7e498c24e1b838807f799fc
timrusin <timrusin@gmail.com>
2022-01-20 20:11:39 -0600

    characters rendering with no errors

--- application/src/Pages/Characters.jsx
+++ application/src/Pages/Characters.jsx
@@ -6,21 +6,23 @@ import { NavItem } from "reactstrap";
 const Characters = ()=>{
   
    const [characters, setCharacters]= useState()
+
      useEffect(()=> {
         fetch('https://swapi.dev/api/people/')
         .then((res)=>res.json())
-        .then((json)=>{
+        .then((json)=>{ 
             setCharacters(json)
         })
-    },(1))
+    },[])
     if(!characters) {
         return <p>Loading.......</p>
     }
-    console.log(characters.results)
-  
+
+    const charactersArray = characters.results
+    
     return(
     <div className="card-container">
-      {characters.results.map(item=> {
+      {charactersArray.map(item=> {
           return (
         <div className="characters-list">
           <CharacterCard {...item} key={item.name}/>
