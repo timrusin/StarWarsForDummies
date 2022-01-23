e513ea096d86498c81b27116a744fce208501cb6
timrusin <timrusin@gmail.com>
2022-01-22 19:16:02 -0600

    finally feel good about home scroll

--- application/src/Components/Navigation.jsx
+++ application/src/Components/Navigation.jsx
@@ -6,25 +6,24 @@ import { Container, Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, 
 
 const Navigation = ()=>{
     return(
-    // <nav>
-    //     <Link to ='/'><div className="Home-Logo">Star Wars: the basics</div></Link>
-    //     <div className="buttons">
-    //     <Link to ='/characters'><button>characters</button></Link>  
-    //     <Link to ='/planets'><button>planets</button></Link>
-    //     <Link to ='/movies'><button>movies</button></Link>
-    //     </div>
-    // </nav>
 <Container>
     <Navbar className="nav-bar" color="warning" expand="md" light>
         <NavbarBrand href="/">
-        StarWars for dummies
+        <span className="star-wars">StarWars</span> <span className="dummies">for dummies</span>
         </NavbarBrand>
         <NavbarToggler onClick={function noRefCheck(){}} />
         <Collapse navbar>
         <Nav className="me-auto" navbar>
+            
+            <NavItem>
+                <NavLink href="/Movies/">
+                Movies
+                </NavLink>
+            </NavItem>
+
             <UncontrolledDropdown inNavbar nav>
             <DropdownToggle caret nav> Characters</DropdownToggle>
-            <DropdownMenu end>
+            <DropdownMenu end className="drop-down">
                 <DropdownItem>
                 <NavLink href='/characters/page1'>Page1</NavLink>
                 </DropdownItem>
@@ -55,36 +54,29 @@ const Navigation = ()=>{
             </DropdownMenu>
             </UncontrolledDropdown>
             
-
-            <NavItem>
-                <NavLink href="/Movies/">
-                Movies
-                </NavLink>
-            </NavItem>
-            
             <UncontrolledDropdown inNavbar nav>
-<DropdownToggle caret nav> Planets</DropdownToggle>
-<DropdownMenu end>
-    <DropdownItem>
-    <NavLink href='/planets/page1'>Page1</NavLink>
-    </DropdownItem>
-    <DropdownItem>
-    <NavLink href='/planets/page2'>Page2</NavLink>
-    </DropdownItem>
-    <DropdownItem>
-    <NavLink href='/planets/page3'>Page3</NavLink>
-    </DropdownItem>
-    <DropdownItem>
-    <NavLink href='/planets/page4'>Page4</NavLink>
-    </DropdownItem>
-    <DropdownItem>
-    <NavLink href='/planets/page5'>Page5</NavLink>
-    </DropdownItem>
-    <DropdownItem>
-    <NavLink href='/planets/page6'>Page6</NavLink>
-    </DropdownItem>
-</DropdownMenu>
-</UncontrolledDropdown>
+            <DropdownToggle caret nav> Planets</DropdownToggle>
+            <DropdownMenu end className="drop-down">
+                <DropdownItem>
+                <NavLink href='/planets/page1'>Page1</NavLink>
+                </DropdownItem>
+                <DropdownItem>
+                <NavLink href='/planets/page2'>Page2</NavLink>
+                </DropdownItem>
+                <DropdownItem>
+                <NavLink href='/planets/page3'>Page3</NavLink>
+                </DropdownItem>
+                <DropdownItem>
+                <NavLink href='/planets/page4'>Page4</NavLink>
+                </DropdownItem>
+                <DropdownItem>
+                <NavLink href='/planets/page5'>Page5</NavLink>
+                </DropdownItem>
+                <DropdownItem>
+                <NavLink href='/planets/page6'>Page6</NavLink>
+                </DropdownItem>
+            </DropdownMenu>
+            </UncontrolledDropdown>
             
             
             
